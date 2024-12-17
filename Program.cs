f1c560f8fdbd488527efef7281e716e7398c11a4
Max-comerit <max.guclu@comerit.se>
2024-12-17 13:37:37 +0100

    State, Routing, LC

--- Program.cs
+++ Program.cs
@@ -10,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<GlobalStateDemo>();
+builder.Services.AddSingleton<GlobalStateNameDemo>();
+
 
 await builder.Build().RunAsync();
---
+++ Service/GlobalStateNameDemo.cs
@@ -0,0 +1,20 @@
+﻿public class GlobalStateNameDemo
+{
+    public string CurrentName { get; private set; } = "Anon";
+
+    public event Action? OnChange;
+
+    public void ChangeName(string newName)
+    {
+        if (!string.IsNullOrWhiteSpace(newName) && CurrentName != newName)
+        {
+            CurrentName = newName;
+            NotifyStateChanged();
+        }
+    }
+
+    private void NotifyStateChanged()
+    {
+        OnChange?.Invoke();
+    }
+}
